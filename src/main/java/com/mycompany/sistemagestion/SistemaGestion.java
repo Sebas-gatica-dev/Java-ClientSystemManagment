5a1a94548849356f71a625afe5a6ecada644e8c4
Sebastian Gatica <expandiendoartemusical@gmail.com>
2023-03-06 00:28:09 -0300

    second commit

---
+++ src/main/java/com/mycompany/sistemagestion/SistemaGestion.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemagestion;
+
+import com.mycompany.sistemagestion.forms.GestionClientes;
+
+/**
+ *
+ * @author Usuario
+ */
+public class SistemaGestion {
+
+    public static void main(String[] args) {
+        GestionClientes ventana = new GestionClientes();
+        ventana.show();
+    }
+
+   
+}
